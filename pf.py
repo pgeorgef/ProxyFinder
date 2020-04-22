5d8e2f25192a024667b26f6b7cee981d3513ecde
P.George <fulgerul46@Yahoo.com>
2020-04-22 12:37:14 +0300

    Changed sets of tuples to list of dictionaries in Proxyfinder

--- pf.py
+++ pf.py
@@ -6,7 +6,7 @@ class ProxyFinder:
         Function that returns a set of tuples with proxy hosts and ports from all the proxy websites
             Example: [(192.168.0.1, 80), .....]
         '''
-        proxies = set()
+        proxies = list()
         for website in Websites:
-            proxies.update(website.get_proxies())
+            proxies.extend(website.get_proxies())
         return proxies
